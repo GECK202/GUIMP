461f74270fe4ce888451e17accb1c76ff877f31d
Valeria Karon <vkaron@il-c2.msk.21-school.ru>
2021-04-26 22:59:50 +0300

    add resize

--- src/main.c
+++ src/main.c
@@ -122,15 +122,23 @@ int		main(void)//int ac, char *av[])
 
 //#include <SDL2/SDL.h>
 
+
+#define SHAPE_SIZE 200
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
 SDL_Window *win = NULL;
 SDL_Surface *scr = NULL;
-SDL_Surface *smile = NULL;
+//SDL_Surface *smile = NULL;
 SDL_Window *win2 = NULL;
 SDL_Surface *scr2 = NULL;
-SDL_Surface *smile2 = NULL;
+//SDL_Surface *smile2 = NULL;
+
+SDL_Renderer *ren = NULL;
+SDL_Renderer *ren2 = NULL;
+SDL_Texture *flower = NULL;
+SDL_Texture *flower2 = NULL;
+
 int win1_is_exist = 1;
 int win2_is_exist = 1;
 
@@ -220,6 +228,18 @@ int init() {
         return 1;
     }
     
+
+    ren = SDL_CreateRenderer(win, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (ren == NULL) {
+        return 1;
+    }
+    SDL_SetRenderDrawColor(ren, 0xFF, 0xFF, 0xFF, 0xFF);
+
+    ren2 = SDL_CreateRenderer(win2, -1, SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (ren2 == NULL) {
+        return 1;
+    }
+    SDL_SetRenderDrawColor(ren2, 0xFF, 0xFF, 0xFF, 0xFF);
     scr = SDL_GetWindowSurface(win);
     scr2 = SDL_GetWindowSurface(win2);
     
@@ -227,22 +247,43 @@ int init() {
 }
 
 int load() {
-    smile = IMG_Load("res/im1.png");
-    smile2 = IMG_Load("res/im2.jpeg");
+    SDL_Surface *smile = IMG_Load("res/im1.png");
+    SDL_Surface *smile2 = IMG_Load("res/im2.jpeg");
     if (smile == NULL) {
         return 1;
     }
     if (smile2 == NULL) {
         return 1;
     }
+
+
+    
+    flower = SDL_CreateTextureFromSurface(ren, smile);
+    if (flower == NULL) {
+        return 1;
+    }
+    flower2 = SDL_CreateTextureFromSurface(ren2, smile2);
+    if (flower2 == NULL) {
+        return 1;
+    }
     return 0;
 }
 
 void quit() {
-    SDL_FreeSurface(smile);
-    SDL_FreeSurface(smile2);
-    smile = NULL;
-    smile2 = NULL;
+    //SDL_FreeSurface(smile);
+    //SDL_FreeSurface(smile2);
+    //smile = NULL;
+    //smile2 = NULL;
+
+    SDL_DestroyTexture(flower);
+    flower = NULL;
+    SDL_DestroyTexture(flower2);
+    flower2 = NULL;
+
+    SDL_DestroyRenderer(ren);
+    ren = NULL;
+    SDL_DestroyRenderer(ren2);
+    ren2 = NULL;
     
     if (win1_is_exist)
 	    SDL_DestroyWindow(win);
@@ -251,11 +292,12 @@ void quit() {
 	    SDL_DestroyWindow(win2);
     
     SDL_Quit();
+    IMG_Quit();
 }
-
+/*
 void cls_win(SDL_Window *window)
 {
-    /* Declaring the surface. */
+    
     SDL_Surface *s;
 
     int width;
@@ -263,31 +305,90 @@ void cls_win(SDL_Window *window)
 
     Uint32 ID;
 
-    SDL_Surface *win_scr = SDL_GetWindowSurface(window);
+    SDL_Surface *screen;
+
 
     SDL_GetWindowSize(window, &width, &height);
 
-    /* Creating the surface. */
+
+ 
     s = SDL_CreateRGBSurface(0, width, height, 32, 0, 0, 0, 0);
 
-    /* Filling the surface with red color. */
+   
     SDL_FillRect(s, NULL, SDL_MapRGB(s->format, 255, 0, 0));
 
-    SDL_BlitSurface(s, NULL, win_scr, NULL);
+    
+
 
     ID = SDL_GetWindowID(window);
+    screen = SDL_GetWindowSurface(window);
+
+    SDL_BlitSurface(s, NULL, screen, NULL);
 
     if (ID == 1)
     {
-        SDL_BlitSurface(smile, NULL, scr, NULL);
-        SDL_UpdateWindowSurface(win);
+        SDL_BlitSurface(smile, NULL, screen, NULL);
     }
     else if (ID == 2)
     {
-        SDL_BlitSurface(smile2, NULL, scr2, NULL);
-        SDL_UpdateWindowSurface(win2);
+        SDL_BlitSurface(smile2, NULL, screen, NULL);
     }
 
+
+    SDL_FreeSurface(s);
+
+    SDL_UpdateWindowSurface(window);
+
+}
+
+//*/
+
+
+static int resizingEventWatcher(void *data, SDL_Event *event) {
+    if (event->type == SDL_WINDOWEVENT && event->window.event == SDL_WINDOWEVENT_RESIZED) {
+        SDL_Window *wind = SDL_GetWindowFromID(event->window.windowID);
+        if (wind == (SDL_Window*)data) {
+        //write(1,"changing...\n",12);
+        //cls_win(wind);
+
+        SDL_Rect SrcR;
+        SDL_Rect DestR;
+        SDL_Rect DestR2;
+
+        SrcR.x = 0;
+        SrcR.y = 0;
+        SrcR.w = SHAPE_SIZE;
+        SrcR.h = SHAPE_SIZE;
+
+
+        //int width;
+        //int height;
+
+        if (event->window.windowID == 1)
+        {
+            //SDL_GetWindowSize(win, &width, &height);
+            DestR.x = 0;//width / 2 - SHAPE_SIZE / 2;
+            DestR.y = 0;//height / 2 - SHAPE_SIZE / 2;
+            DestR.w = SHAPE_SIZE;
+            DestR.h = SHAPE_SIZE;
+            SDL_RenderClear(ren);
+            SDL_RenderCopy(ren, flower, &SrcR, &DestR);
+            SDL_RenderPresent(ren);
+        }
+        else
+        {
+            //SDL_GetWindowSize(win2, &width, &height);
+            DestR2.x = 0;//width / 2 - SHAPE_SIZE / 2;
+            DestR2.y = 0;//height / 2 - SHAPE_SIZE / 2;
+            DestR2.w = SHAPE_SIZE;
+            DestR2.h = SHAPE_SIZE;
+            SDL_RenderClear(ren2);
+            SDL_RenderCopy(ren2, flower2, &SrcR, &DestR2);
+            SDL_RenderPresent(ren2);
+        }
+    }
+}
+    return 0;
 }
 
 int main (void) {//int argc, char ** args) {
@@ -299,13 +400,20 @@ int main (void) {//int argc, char ** args) {
         return 1;
     }
     write(1,"2\n",2);
-    SDL_BlitSurface(smile, NULL, scr, NULL);
-    SDL_BlitSurface(smile2, NULL, scr2, NULL);
-    write(1,"3\n",2);
-    SDL_UpdateWindowSurface(win);
-    SDL_UpdateWindowSurface(win2);
-    write(1,"4\n",2);
-    SDL_Delay(2000);
+
+    SDL_AddEventWatch(resizingEventWatcher, win);
+    SDL_AddEventWatch(resizingEventWatcher, win2);
+
+//SDL_Surface *screen = SDL_GetWindowSurface(win);
+//SDL_Surface *screen2 = SDL_GetWindowSurface(win2);
+
+    //SDL_BlitSurface(smile, NULL, scr, NULL);
+    //SDL_BlitSurface(smile2, NULL, scr2, NULL);
+    //write(1,"3\n",2);
+    //SDL_UpdateWindowSurface(win);
+    //SDL_UpdateWindowSurface(win2);
+    //write(1,"4\n",2);
+    //SDL_Delay(2000);
     SDL_Event windowEvent;
     
     while ( 1 )
@@ -328,14 +436,23 @@ int main (void) {//int argc, char ** args) {
         		if (win1_is_exist == 0 && win2_is_exist == 0)
 	        		break;
         	}
+/*
+            if ( SDL_WINDOWEVENT_SIZE_CHANGED == windowEvent.window.event)
+            {
+                if (windowEvent.window.windowID == 1)
+                    cls_win(win);
+                else if (windowEvent.window.windowID == 2)
+                    cls_win(win2);
+            }
 
-            //if ( SDL_WINDOWEVENT_SIZE_CHANGED == windowEvent.window.event)
-            //{
-            //    if (windowEvent.window.windowID == 1)
-            //        cls_win(win);
-            //    else if (windowEvent.window.windowID == 2)
-            //        cls_win(win2);
-            //}
+            if ( SDL_WINDOWEVENT_RESIZED == windowEvent.window.event)
+            {
+                if (windowEvent.window.windowID == 1)
+                    cls_win(win);
+                else if (windowEvent.window.windowID == 2)
+                    cls_win(win2);
+            }
+            //*/
 
         	if ( SDL_TEXTINPUT == windowEvent.text.type)
         	{
@@ -349,13 +466,61 @@ int main (void) {//int argc, char ** args) {
         		SDL_MouseButtonEvent m_ev;
         		m_ev = windowEvent.button;
 
-        		SDL_Log("Window %d mouse button %d down in coord[%d:%d] clicks=%d", windowEvent.window.windowID, m_ev.button, m_ev.x, m_ev.y, m_ev.clicks);
+                SDL_Log("Window %d mouse button %d down in coord[%d:%d] clicks=%d", windowEvent.window.windowID, m_ev.button, m_ev.x, m_ev.y, m_ev.clicks);
         	}
+
+
             if ( SDL_QUIT == windowEvent.type )
             {
                 break;
             }
         }
+
+
+
+        SDL_Rect SrcR;
+        SDL_Rect DestR;
+        SDL_Rect DestR2;
+
+        SrcR.x = 0;
+        SrcR.y = 0;
+        SrcR.w = SHAPE_SIZE;
+        SrcR.h = SHAPE_SIZE;
+
+
+        //int width;
+        //int height;
+
+        if (windowEvent.window.windowID == 1)
+        {
+            //SDL_GetWindowSize(win, &width, &height);
+            DestR.x = 0;//width / 2 - SHAPE_SIZE / 2;
+            DestR.y = 0;//height / 2 - SHAPE_SIZE / 2;
+            DestR.w = SHAPE_SIZE;
+            DestR.h = SHAPE_SIZE;
+            SDL_RenderClear(ren);
+            SDL_RenderCopy(ren, flower, &SrcR, &DestR);
+            SDL_RenderPresent(ren);
+        }
+        else
+        {
+            //SDL_GetWindowSize(win2, &width, &height);
+            DestR2.x = 0;//width / 2 - SHAPE_SIZE / 2;
+            DestR2.y = 0;//height / 2 - SHAPE_SIZE / 2;
+            DestR2.w = SHAPE_SIZE;
+            DestR2.h = SHAPE_SIZE;
+            SDL_RenderClear(ren2);
+            SDL_RenderCopy(ren2, flower2, &SrcR, &DestR2);
+            SDL_RenderPresent(ren2);
+        }
+
+        
+
+
+        
+
+        
+
     }
     write(1,"5\n",2);
     quit();
